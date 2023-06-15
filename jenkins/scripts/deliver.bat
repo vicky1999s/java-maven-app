c77162e9451814308334e09ed52d7681d90a812f
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2023-06-15 16:14:48 +0530

    edited bat script

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -4,23 +4,22 @@ echo The following Maven command installs your Maven-built Java application
 echo into the local Maven repository, which will ultimately be stored in
 echo Jenkins's local Maven repository
 
-setlocal enabledelayedexpansion
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
+call mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set "NAME=!output!"
 
-echo The following complex command extracts the value of the <name/> element
-echo within <project/> of your Java/Maven project's "pom.xml" file.
+@REM echo The following complex command extracts the value of the <name/> element
+@REM echo within <project/> of your Java/Maven project's "pom.xml" file.
 
-mvn help:evaluate -Dexpression=project.name | findstr /r /v "^$" > output.txt
-set /p NAME=<output.txt
+@REM mvn help:evaluate -Dexpression=project.name | findstr /r /v "^$" > output.txt
+@REM set /p NAME=<output.txt
 
-echo The following complex command behaves similarly to the previous one but
-echo extracts the value of the <version/> element within <project/> instead.
+@REM echo The following complex command behaves similarly to the previous one but
+@REM echo extracts the value of the <version/> element within <project/> instead.
 
-mvn help:evaluate -Dexpression=project.version | findstr /r /v "^$" > output.txt
-set /p VERSION=<output.txt
+@REM mvn help:evaluate -Dexpression=project.version | findstr /r /v "^$" > output.txt
+@REM set /p VERSION=<output.txt
 
 echo The following command runs and outputs the execution of your Java
 echo application (which Jenkins built using Maven) to the Jenkins UI.
 
-java -jar target\!NAME!-!VERSION!.jar
+java -jar target\my-app-1.0-SNAPSHOT.jar
