bd59aed3a303dac7a9f1d0e6e87ff68207f62f7d
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2023-06-16 12:55:01 +0530

    changed App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Vicky!";
+    private static final String MESSAGE = "Hello Vicky test!";
 
     public App() {}
 
