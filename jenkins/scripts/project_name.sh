04d6a654e37f2f27ce49e35e79a0fe661ff1b0b4
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2023-06-22 16:11:10 +0530

    added project_name script and edited Jenkinsfile

---
+++ jenkins/scripts/project_name.sh
@@ -0,0 +1,5 @@
+MONTH=$(date "+%m")
+YEAR=$(data "+%Y")
+PROJECT_NAME=$(mvn help:evaluate -Dexpression=project.name | grep "^[^\[]")
+PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "^[^\[]")
+FILE_NAME=${PROJECT_NAME}-${PROJECT_VERSION}.jar
